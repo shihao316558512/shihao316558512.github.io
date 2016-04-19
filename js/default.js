e39f475d6f31420d8a41614bf8d06dc2c0157e79
ShiHao <shihao316558512@hotmail.com>
2016-04-19 19:17:10 +0800

    添加fork me

---
+++ js/default.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    setScrollToTop();
+    $(window).resize(function () {
+        setScrollToTop();
+    })
+    $(window).scroll(function () {
+        setScrollToTop();
+    })
+    wow = new WOW(
+      {
+          animateClass: 'animated',
+          offset: 100,
+          callback: function (box) {
+              console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+          }
+      }
+    );
+    wow.init();
+})
+function setScrollToTop() {
+    var top = $(window).scrollTop();
+    if (top < 60) {
+        $('#goTop').hide();
+    } else {
+        $('#goTop').show();
+        console.log("show");
+    }
+    console.log("show");
+}
