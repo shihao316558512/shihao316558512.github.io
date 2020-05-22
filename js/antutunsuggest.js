607b7622e7588f42306792029d20dd7f6b761f2b
shihao <nms-89@DESKTOP-J2SHV3O>
2020-05-22 11:23:10 +0800

    新增新机榜和推荐榜

---
+++ js/antutunsuggest.js
@@ -0,0 +1,5 @@
+﻿var data = [
+    { "name": "xiaomi10", "time": "2020/5/1", "score": "21000", "price": "1988", "href": "#", "content": "简约大气" },
+    { "name": "xiaomi11", "time": "2020/5/2", "score": "21000", "price": "1588", "href": "#", "content": "简约大气" }
+
+];
