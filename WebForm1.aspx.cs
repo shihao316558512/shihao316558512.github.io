4e5eea7d7da5f99dede32c905a02dc8d5d1adbf7
shihao <nms-89@DESKTOP-J2SHV3O>
2020-07-02 09:29:09 +0800

    增加Redmi 9

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -15,23 +15,25 @@ namespace shihao316558512.github.io
         protected void Page_Load(object sender, EventArgs e)
         {
             string a = @"
-	--2020年6月														
+	--2020年6月
+			Redmi 9			2020年6月24		799		200000			https://u.jd.com/pD2YZz		198	4g	5020	18		联发科g80中低端cpu，入门机
 			oppo Reno4		2020年6月5日		2999		340000			https://u.jd.com/Kqk0bq		183	5g	4020	65		高通765G中端cpu，2.5D打孔曲面屏，有nfc，主打视频拍摄
 			oppo Reno4 Pro		2020年6月5日		3799		340000			https://u.jd.com/IpO6rG		172	5g	4000	65		高通765G中端cpu，90Hz刷新率3D曲面屏，有nfc
-			荣耀Play4		2020年6月3日		1799		315000			https://u.jd.com/GnmRC3		213	5g	4300	22.5		天玑800中端cpu，Lcd单挖孔屏
+			荣耀Play4		2020年6月3日		1799		315000			https://u.jd.com/GnmRC3		213	5g	4300	22.5		联发科天玑800中端cpu，Lcd单挖孔屏
 			荣耀Play4Pro		2020年6月3日		2899		480000			https://u.jd.com/Srr9ef		213	5g	4200	40		麒麟990外挂5g顶级cpu，lcd双挖孔屏，无接触红外测温，有nfc
 			vivoX50			2020年6月1日		3498		334135			https://u.jd.com/0X0ssv		174	5g	4200	33		高通765g中端cpu，90hz超薄柔性屏，有nfc
 			vivoX50 Pro		2020年6月1日		4298		334135			https://u.jd.com/gTdHid		181	5g	4315	33		高通765g中端cpu，90hz轻薄曲面屏，主打微云台超感光主摄，nfc
-			vivoX50 Pro+		2020年6月1日		4998		600000			7月开售				174	5g	4350	44		高通865顶级cpu，120hz曲面屏，nfc
+			vivoX50 Pro+		2020年6月1日		4998		600000			https://u.jd.com/J8nNQq		174	5g	4350	44		高通865顶级cpu，120hz曲面屏，nfc
+			Axon11 SE		2020年6月1号		1998		334352			https://u.jd.com/c3mDNp		184	5g	4000	18		联发科天玑800中端cpu
 			--2020年5月														          	    	
 			vivoY70s		2020年5月26日		1998		288005			https://u.jd.com/JLQG6D		190	5g	4500	18		三星Exynos880中端cpu，Lcd打孔屏
-			Redmi 10X pro（5g版）	2020年5月26日		2299		410234			https://u.jd.com/2GgcIM		208	5g	4520	33		天玑820中高端cpu，三星AMOLED屏幕，z轴线性马达，有nfc
-			Redmi 10X（5g版）	2020年5月26日		1599		410209			https://u.jd.com/6XwLz2		205	5g	4520	22.5		天玑820中高端cpu，三星AMOLED屏幕，z轴线性马达，建议至少买6+128版本
+			Redmi 10X pro（5g版）	2020年5月26日		2299		410234			https://u.jd.com/2GgcIM		208	5g	4520	33		联发科天玑820中高端cpu，三星AMOLED屏幕，z轴线性马达，有nfc
+			Redmi 10X（5g版）	2020年5月26日		1599		410209			https://u.jd.com/6XwLz2		205	5g	4520	22.5		联发科天玑820中高端cpu，三星AMOLED屏幕，z轴线性马达，建议至少买6+128版本
 			Redmi 10X（4g版）	2020年5月26日		999		208493			https://u.jd.com/DJczHK		199	4g	5020	18		9w无线反向充电，联发科g85低端cpu，Lcd挖孔屏，适合父母用
 			realme X50 Pro玩家版	2020年5月25日		2699		594706			https://u.jd.com/OPBhLZ		209	5g	4200	65		高通865顶级cpu，90hz三星Super AMOLED屏幕，z轴线性马达
-			华为畅享z		2020年5月24日		1699		373377			https://u.jd.com/g4J6jW		182	5g	4000	22.5		天玑800中端cpu，90hzLCD屏幕
+			华为畅享z		2020年5月24日		1699		373377			https://u.jd.com/g4J6jW		182	5g	4000	22.5		联发科天玑800中端cpu，90hzLCD屏幕
 			荣耀x10			2020年5月20日		1899		373377			https://u.jd.com/XgwBrZ		203	5g	4300	22.5		麒麟820中高端cpu，Lcd 90hz屏幕，升降摄像头，建议至少买6+128版本
-			iqooz1			2020年5月19日		2198		526401			https://u.jd.com/IoU84V		194	5g	4500	44		天玑1000+高端cpu，Lcd 144hz屏幕，vivo子品牌，性价比高
+			iqooz1			2020年5月19日		2198		526401			https://u.jd.com/IoU84V		194	5g	4500	44		联发科天玑1000+高端cpu，Lcd 144hz屏幕，vivo子品牌，性价比高
 			Redmi K30极速版		2020年5月11日		1999		349194			https://u.jd.com/8qiWS9		208	5g	4500	30		高通768g中高端cpu，Lcd 120hz刷新率屏幕，nfc 
 			魅族17			2020年5月8日		3699		577763			https://u.jd.com/VpVUat		199	5g	4500	30		高通865顶级cpu，顶级配置，x轴线性马达，nfc
 			魅族17 Pro		2020年5月8日 		4299		590826			https://u.jd.com/uOarQZ		219	5g	4500	30		27w无线快充，高通865高端cpu，顶级配置，x轴线性马达，nfc
@@ -42,7 +44,7 @@ namespace shihao316558512.github.io
 			华为Nova7SE		2020年4月23日		2799   		363923			https://u.jd.com/1VaAvl		189	5g	4000	40		麒麟820中端cpu，适合学生，女生
 			华为nova7		2020年4月23日		3399   		404215			https://u.jd.com/CFmAyc		180	5g	4000	40		麒麟985中高端cpu，没有高刷新率屏幕，性能略微强于麒麟820，nfc，拍照好
 			华为Nova7Pro		2020年4月23日		4099		401428			https://u.jd.com/NaHQJL		178	5g	4000	40		麒麟985中高端cpu，比nova7拍照更好，曲面屏，nfc
-			OPPO A92s		2020年4月21日		2199		295798 			https://u.jd.com/alAs9v		184	5g	4000	18		天玑800中端cpu，Lcd 120hz屏幕，
+			OPPO A92s		2020年4月21日		2199		295798 			https://u.jd.com/alAs9v		184	5g	4000	18		联发科天玑800中端cpu，Lcd 120hz屏幕，
 			努比亚Play5G		2020年4月21日		2399  		330459			https://u.jd.com/w6bkne		210	5g	5100	30		高通765g中端处理器，144hz刷新率AMOLED屏幕，只是近几年这个品牌没有好好经营，没落了
 			iPhoneSE2		2020年4月18日		3299  		468486			https://u.jd.com/L4O8ev		148	4g	1821	5		外观与iPhone8几乎一样，cpu是最新的a13，性能好，但续航极差，不建议买
 			一加8			2020年4月17日		3999  		580460			https://u.jd.com/PyU04P		180	5g	4300	30		高通865高端cpu，一加正统数字系列年度旗舰，配置是目前顶级，系统十分流畅，但有人说系统不如小米和华为完善
@@ -68,7 +70,7 @@ namespace shihao316558512.github.io
 			OPPO Find X2 Pro	2020年3月6日		6999		607147			https://u.jd.com/mWLXNM		200	5g	4260	65		高通865顶级cpu，AMOLED120hz QHD+屏幕，nfc，X轴线性马达
 			腾讯黑鲨游戏手机3	2020年3月3日		3499		570680			https://u.jd.com/s2aemc		222	5g	4720	65		高通865顶级cpu，三星AMOLED 90hz屏幕，z轴线性马达
 			腾讯黑鲨游戏手机3 Pro	2020年3月3日		4699		586062			https://u.jd.com/nbkqbu		253	5g	5000	65		高通865顶级cpu，三星AMOLED QHD+ 90hz屏幕，x轴线性马达
-			华为畅享10e		2020年3月1日		999		87624			https://u.jd.com/abDjsK		185	4g	5000	10		mtk p35垃圾cpu，不建议买
+			华为畅享10e		2020年3月1日		999		87624			https://u.jd.com/abDjsK		185	4g	5000	10		联发科p35垃圾cpu，不建议买
 			--2020年2月														          	    	  		
 			vivo Z6			2020年2月29日		2298 		323684			https://u.jd.com/QEJLe9		201	5g	5000	44		高通765g中端cpu，lcd单挖孔屏幕
 			三星S20			2020年2月27日 		6999   		556393			https://u.jd.com/Umj06e		163	5g	4000	25		高通865顶级cpu，年度旗舰配置，闭眼买
@@ -81,7 +83,7 @@ namespace shihao316558512.github.io
 			--2020年1月														          	    	  		
 			realme X50（5G）	2020年1月7日		2499		330747			https://u.jd.com/airWHM		202	5g	4100	30		高通765g中端cpu，lcd双挖孔屏，nfc
 			--2019年12月														          	    	  		
-			oppo Reno3		2020年12月26日		3399		405159			https://u.jd.com/DVMs23		181	5g	4025	30		天玑1000L中高端cpu，nfc
+			oppo Reno3		2020年12月26日		3399		405159			https://u.jd.com/DVMs23		181	5g	4025	30		联发科天玑1000L中高端cpu，nfc
 			oppo Reno3 Pro		2020年12月26日		3999		319937			https://u.jd.com/U6l8hl		171	5g	4025	30		高通765G中端cpu，90Hz曲面屏，nfc
 			vivo X30		2019年12月16日		3298		327092			https://u.jd.com/PgTVgL		198	5g	4350	33		三星Exynos980中端cpu，
 			vivo X30 Pro		2019年12月16日		3998		327092			https://u.jd.com/sJMr2o		198	5g	4350	33		三星Exynos980中端cpu，5倍潜望式长焦镜头
@@ -98,6 +100,7 @@ namespace shihao316558512.github.io
 			--2019年8														          	    	  		
 			Redmi Note8		2019年8月29日		899		175623			https://u.jd.com/h1ASv6		190	4g	4000	18		高通665低端cpu，1000内可选
 			Redmi Note8 pro		2019年8月29日		1399		289559			https://u.jd.com/8J12i2		199	4g	4500	18		联发科g90t中低端cpu，有nfc，水桶机，建议选购8+128版本，目前售价在1500内
+
 ";
             string[] b = a.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
             List<Mobile> mobileList = new List<Mobile>();
